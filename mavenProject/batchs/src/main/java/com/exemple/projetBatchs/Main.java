b4708e318c307b908d41368a39102bf41a6c15ca
polo92 <zizopolo92@gmail.com>
2018-06-25 11:06:06 +0000

    first commit

---
+++ mavenProject/batchs/src/main/java/com/exemple/projetBatchs/Main.java
@@ -0,0 +1,14 @@
+package com.exemple.projetBatchs;
+
+public class Main {
+
+    /*
+     * creat by polo zizo
+     */
+    public static void main(String[] args){
+
+        System.out.println( "hello developper :-)" );
+
+    }
+
+}
---
+++ mavenProject/modules/src/main/java/com/exemple/projetModules/App.java
@@ -0,0 +1,13 @@
+package com.exemple.projetModules;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
